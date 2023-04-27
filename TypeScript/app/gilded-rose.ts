f4bf2198824e1ee44b9fe250598f685aacbaf7f3
Damiano Salvi <salvi@intre.it>
2023-04-27 17:30:05 +0200

    Extract constants

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,10 @@ export class Item {
   }
 }
 
+const brieName = 'Aged Brie';
+const passName = 'Backstage passes to a TAFKAL80ETC concert';
+const sulfurasName = 'Sulfuras, Hand of Ragnaros';
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -19,16 +23,16 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != brieName && this.items[i].name != passName) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != sulfurasName) {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name == passName) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
@@ -42,14 +46,14 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != sulfurasName) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != brieName) {
+          if (this.items[i].name != passName) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != sulfurasName) {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
